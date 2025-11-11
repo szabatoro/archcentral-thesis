de7f59805cf49e05c636f53ae8edfdb82e4b3e35
szabatoro <99820354+szabatoro@users.noreply.github.com>
2025-11-11 11:37:42 +0100

    version bump

--- archcentral/modules/archcentral.py
+++ archcentral/modules/archcentral.py
@@ -5,7 +5,7 @@ from archcentral.modules.packagemanager import PackageManagerModule
 from archcentral.ui.designer.mainwindow import Ui_MainWindow
 import sys
 
-VERSION: str = "0.0.2"
+VERSION: str = "0.0.3"
 
 # Main window of the application. All the modules will be loaded within this window
 class MainWindow(QMainWindow, Ui_MainWindow):
