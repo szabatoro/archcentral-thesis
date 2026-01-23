64ab16153ee0c2e63678ef3e9b29d03482e7b9e9
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-01-23 14:01:51 +0100

    added model for the update table view

---
+++ archcentral/models/pacman_update_list.py
@@ -0,0 +1,30 @@
+from PySide6.QtCore import QAbstractTableModel, Qt
+
+# Model for the update table
+class PacmanUpdateTableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+        self._headers = ["Package", "Installed Version", "New Version"]
+
+    def rowCount(self, parent=None):
+        return len(self._data)
+
+    def columnCount(self, parent=None):
+        return len(self._headers)
+
+    def data(self, index, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+        return None
+
+    def get_packagenames(self):
+        return [row[0] for row in self._data]
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self._headers[section]
+            else:
+                return str(section + 1)
+        return None
