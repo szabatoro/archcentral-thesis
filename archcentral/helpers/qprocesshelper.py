1378a94eef776a8364bb7d2a4c27a77e0f024624
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-01-23 14:00:09 +0100

    changed qprocesshelper to support streaming stdout

--- archcentral/helpers/qprocesshelper.py
+++ archcentral/helpers/qprocesshelper.py
@@ -1,27 +1,36 @@
-from PySide6.QtCore import QByteArray, QProcess, QObject,  Signal
+from PySide6.QtCore import QByteArray, QProcess, QObject, Signal
 
-# Wrapper around QProcess to make getting cli output easier
 class QProcessHandler(QObject):
     finished: Signal = Signal(str)
+    stream: Signal = Signal(str)  # new streaming signal
 
     def __init__(self) -> None:
         super().__init__()
         self.process: QProcess = None
+        self._buffer = ""  # optional, in case you want to collect full output
 
-    # Start the given process with the given arguments
     def start_process(self, program, arguments) -> None:
         if self.process is None:
             self.process = QProcess()
+            self.process.readyReadStandardOutput.connect(self._read_stdout)  # stream output
             self.process.finished.connect(self._handle_finished)
             self.process.start(program, arguments)
 
-    # Emit finish signal with decoded process stdout
-    def _handle_finished(self) -> None:
+    def _read_stdout(self) -> None:
         if self.process:
             data: QByteArray = self.process.readAllStandardOutput()
             if data:
-                output: str = bytes(data).decode("utf8").strip()
-                self.finished.emit(output)
-            else:
-                self.finished.emit("")
+                output: str = bytes(data).decode("utf8")
+                self._buffer += output  # keep a running buffer
+                self.stream.emit(output)  # stream immediately
+
+    def _handle_finished(self) -> None:
+        if self.process:
+            # emit any remaining output
+            remaining_data: QByteArray = self.process.readAllStandardOutput()
+            if remaining_data:
+                self._buffer += bytes(remaining_data).decode("utf8")
+            # emit final combined output
+            self.finished.emit(self._buffer.strip())
         self.process = None
+        self._buffer = ""
