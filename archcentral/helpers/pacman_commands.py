f0b9cf361699720510b2500a177f31dc874e9ee3
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-01-24 15:07:57 +0100

    introduced pyalpm for read-only tasks

--- archcentral/helpers/pacman_commands.py
+++ archcentral/helpers/pacman_commands.py
@@ -1,9 +1,31 @@
 from PySide6.QtCore import QObject,  Signal
 from archcentral.helpers.qprocesshelper import QProcessHandler
+import pyalpm
+import re
 
-class PacmanUpdater(QObject):
+class PacmanManager(QObject):
     finished_fetch: Signal = Signal(list)
     running_update: Signal = Signal(str)
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Read the pacman config file
+        self.pacman_conf = open("/etc/pacman.conf", "r")
+        self.pacman_conf_str: str = self.pacman_conf.read()
+        self.pacman_conf.close()
+
+        # Extract enabled repos from pacman config
+        self.enabled_repos = re.findall(r'^(?!#)(?!.*option)\[(.*)\]$', self.pacman_conf_str, re.MULTILINE)
+
+        # Initialize handle and get localdb
+        self.handle = pyalpm.Handle(".", "/var/lib/pacman")
+        self.localdb = self.handle.get_localdb()
+
+        # Load remote repository dbs
+        for repo in self.enabled_repos:
+            self.handle.register_syncdb(f"{repo}", pyalpm.SIG_DATABASE_OPTIONAL)
+        self.syncdbs = self.handle.get_syncdbs()
 
     # fetches latest package info from mirrors
     def fetch(self) -> None:
@@ -11,26 +33,20 @@ class PacmanUpdater(QObject):
         Fetches latest package info from mirrors, lists updateable packages and emits them with the finished_fetch signal.
         """
         self.pacman_worker = QProcessHandler()
-        self.pacman_worker.finished.connect(self._get_update_list)
+        self.pacman_worker.finished.connect(self._get_update_list_fr)
         self.pacman_worker.start_process("pkexec", ["pacman", "-Sy"])
 
-    # reads updateable packages
-    def _get_update_list(self) -> None:
-        self.pacman_worker = QProcessHandler()
-        self.pacman_worker.finished.connect(self._process_pacman_fetch_output)
-        self.pacman_worker.start_process("pacman", ["-Qu"])
-
-    # processes updateable packages into a list
-    def _process_pacman_fetch_output(self, raw_updates) -> None:
-        updates: list = raw_updates.splitlines()
-
-        processed_updates: list = []
-        for upd in updates:
-            processed_update: str = upd.split()
-            processed_update.remove('->')
-            processed_updates.append(processed_update)
-
-        self.finished_fetch.emit(processed_updates)
+    # Finds updateable packages using pyalpm
+    def _get_update_list_fr(self) -> None:
+        updateable_packages: list[tuple[str,str,str]] = []
+        for pkg in self.localdb.pkgcache:
+            for db in self.syncdbs:
+                sync_pkg = db.get_pkg(pkg.name)
+                if sync_pkg:
+                    if pyalpm.vercmp(sync_pkg.version, pkg.version) == 1:
+                        updateable_packages.append((pkg.name, pkg.version, sync_pkg.version, sync_pkg.size))
+                    break
+        self.finished_fetch.emit(updateable_packages)
 
     def update(self, packagelist) -> None:
         """
