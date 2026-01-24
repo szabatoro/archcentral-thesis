fc1bb3f53e75ac17a725ce9841beabc8fa4c4c18
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-01-24 15:06:56 +0100

    added docstring to qprocesshelper

--- archcentral/helpers/qprocesshelper.py
+++ archcentral/helpers/qprocesshelper.py
@@ -2,17 +2,20 @@ from PySide6.QtCore import QByteArray, QProcess, QObject, Signal
 
 class QProcessHandler(QObject):
     finished: Signal = Signal(str)
-    stream: Signal = Signal(str)  # new streaming signal
+    stream: Signal = Signal(str)
 
     def __init__(self) -> None:
         super().__init__()
         self.process: QProcess = None
-        self._buffer = ""  # optional, in case you want to collect full output
+        self._buffer = ""
 
     def start_process(self, program, arguments) -> None:
+        """
+        Uses QProcess() to start a program with its arguments and continously logs stdout.
+        """
         if self.process is None:
             self.process = QProcess()
-            self.process.readyReadStandardOutput.connect(self._read_stdout)  # stream output
+            self.process.readyReadStandardOutput.connect(self._read_stdout)
             self.process.finished.connect(self._handle_finished)
             self.process.start(program, arguments)
 
@@ -21,8 +24,8 @@ class QProcessHandler(QObject):
             data: QByteArray = self.process.readAllStandardOutput()
             if data:
                 output: str = bytes(data).decode("utf8")
-                self._buffer += output  # keep a running buffer
-                self.stream.emit(output)  # stream immediately
+                self._buffer += output
+                self.stream.emit(output)
 
     def _handle_finished(self) -> None:
         if self.process:
