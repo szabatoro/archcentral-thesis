19da639ffca187133af36028d11cb3f7ab00cc15
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-01-27 15:43:24 +0100

    created basic layout and functionality for package manager install/remove section

--- archcentral/ui/designer/packagemanager.py
+++ archcentral/ui/designer/packagemanager.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QPlainTextEdit,
-    QPushButton, QSizePolicy, QTabWidget, QTableView,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QLabel,
+    QLineEdit, QPlainTextEdit, QPushButton, QSizePolicy,
+    QTabWidget, QTableView, QVBoxLayout, QWidget)
 
 class Ui_PackageManager(object):
     def setupUi(self, PackageManager):
@@ -53,6 +53,7 @@ class Ui_PackageManager(object):
 
         self.update_button = QPushButton(self.update)
         self.update_button.setObjectName(u"update_button")
+        self.update_button.setEnabled(False)
 
         self.verticalLayout_2.addWidget(self.update_button)
 
@@ -68,6 +69,63 @@ class Ui_PackageManager(object):
         font1 = QFont()
         font1.setBold(True)
         self.management.setFont(font1)
+        self.verticalLayout_3 = QVBoxLayout(self.management)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalWidget_2 = QWidget(self.management)
+        self.horizontalWidget_2.setObjectName(u"horizontalWidget_2")
+        self.horizontalWidget_2.setMaximumSize(QSize(16777215, 30))
+        self.horizontalLayout_2 = QHBoxLayout(self.horizontalWidget_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.package_search = QLineEdit(self.horizontalWidget_2)
+        self.package_search.setObjectName(u"package_search")
+
+        self.horizontalLayout_2.addWidget(self.package_search)
+
+        self.pushButton_3 = QPushButton(self.horizontalWidget_2)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.horizontalLayout_2.addWidget(self.pushButton_3)
+
+
+        self.verticalLayout_3.addWidget(self.horizontalWidget_2)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.package_list_table = QTableView(self.management)
+        self.package_list_table.setObjectName(u"package_list_table")
+
+        self.horizontalLayout.addWidget(self.package_list_table)
+
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.pushButton_2 = QPushButton(self.management)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout_4.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(self.management)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout_4.addWidget(self.pushButton)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_4)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout)
+
+        self.tabWidget = QTabWidget(self.management)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout_3.addWidget(self.tabWidget)
+
         self.content.addTab(self.management, "")
 
         self.verticalLayout.addWidget(self.content)
@@ -75,7 +133,8 @@ class Ui_PackageManager(object):
 
         self.retranslateUi(PackageManager)
 
-        self.content.setCurrentIndex(0)
+        self.content.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(PackageManager)
@@ -88,6 +147,12 @@ class Ui_PackageManager(object):
         self.update_button.setText(QCoreApplication.translate("PackageManager", u"Update", None))
         self.pacman_output.setPlaceholderText(QCoreApplication.translate("PackageManager", u"Pacman update output will be printed here...", None))
         self.content.setTabText(self.content.indexOf(self.update), QCoreApplication.translate("PackageManager", u"Update packages", None))
+        self.package_search.setPlaceholderText(QCoreApplication.translate("PackageManager", u"Search a package...", None))
+        self.pushButton_3.setText(QCoreApplication.translate("PackageManager", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("PackageManager", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("PackageManager", u"PushButton", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("PackageManager", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("PackageManager", u"Tab 2", None))
         self.content.setTabText(self.content.indexOf(self.management), QCoreApplication.translate("PackageManager", u"Install/Remove packages", None))
     # retranslateUi
 
--- archcentral/ui/views/packagemanager.py
+++ archcentral/ui/views/packagemanager.py
@@ -1,6 +1,8 @@
+from PySide6.QtCore import QSortFilterProxyModel
 from PySide6.QtWidgets import QWidget, QDialog
 from archcentral.ui.designer.packagemanager import Ui_PackageManager
 from archcentral.models.pacman_update_list import PacmanUpdateTableModel
+from archcentral.models.pacman_package_list import PacmanPackageListTableModel
 from archcentral.ui.views.pacman_update_dialog import PacmanUpdateDialog
 from archcentral.controllers.packagemanager_controller import PackageManagerController
 
@@ -19,6 +21,12 @@ class PackageManagerModule(QWidget, Ui_PackageManager):
         self.update_table.model = PacmanUpdateTableModel([])
         self.update_table.setModel(self.update_table.model)
 
+        # Set up the table view in the instalL/manage section
+        self.package_list_table.model = PacmanPackageListTableModel([])
+        self.package_list_table.setModel(self.package_list_table.model)
+        self.fill_package_table(self.pmc.list_all_packages())
+        self.package_search.textChanged.connect(self.package_table_search)
+
         # Refresh model data with output of pacman -Qu, blank out previous pacman output
         self.fetch_update_button.clicked.connect(self.pmc.fetch_updates)
         self.fetch_update_button.clicked.connect(lambda: self.pacman_output.setPlainText(""))
@@ -26,13 +34,23 @@ class PackageManagerModule(QWidget, Ui_PackageManager):
         # Update the packages listed in the update table view
         self.update_button.clicked.connect(self.update_packages)
 
-    # Hook up model with fresh data to the update table view
     def fill_update_table(self, updates) -> None:
+        """Initializes the update table widget with the latest model data."""
         self.update_table.model = PacmanUpdateTableModel(updates)
         self.update_table.setModel(self.update_table.model)
 
-    # Gray out update button if there are no packages to update
+    def fill_package_table(self, packagelist) -> None:
+        """Initializes the package table widget with the latest model data."""
+        self.package_list_table.model = PacmanPackageListTableModel(packagelist)
+        self.package_list_table.setModel(self.package_list_table.model)
+
+    def package_table_search(self, search_text) -> None:
+        """Takes a string and filters data in the package list model with it."""
+        proxyModel = QSortFilterProxyModel(self)
+        proxyModel.setSourceModel(self.package_list_table.model)
+
     def are_there_updates(self) -> None:
+        """Checks if there are updates available and sets the state of the update button accordingly."""
         if not self.update_table.model.get_packagenames():
             self.update_button.setText("Up to date")
             self.update_button.setEnabled(False)
@@ -40,13 +58,13 @@ class PackageManagerModule(QWidget, Ui_PackageManager):
             self.update_button.setText("Update")
             self.update_button.setEnabled(True)
 
-    # Set up pacman worker for performing updates
     def update_packages(self) -> None:
+        """Initiates the package update process depending on the dialog box return value."""
         if self.open_update_confirm_dialog():
             self.pmc.perform_update(self.update_table.model.get_packagenames())
 
-    # Opens dialog box for update confirmation
     def open_update_confirm_dialog(self) -> bool:
+        """Opens dialog box for update confirmation. Returns a boolean value depending on if the dialog is accepted or not."""
         dialog: PacmanUpdateDialog = PacmanUpdateDialog(self.update_table.model.get_total_size())
         result = dialog.exec()
         if result == QDialog.Accepted:
