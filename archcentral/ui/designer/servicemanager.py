0c01db131c3f8903a9b4c3ab11a91a639f0b602c
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-02-14 09:29:35 +0100

    implemented basic systemd service ui

--- archcentral/ui/designer/servicemanager.py
+++ archcentral/ui/designer/servicemanager.py
@@ -15,7 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QTabWidget,
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QHBoxLayout, QHeaderView,
+    QLabel, QLineEdit, QPushButton, QSizePolicy,
+    QTabWidget, QTableView, QTableWidget, QTableWidgetItem,
     QVBoxLayout, QWidget)
 
 class Ui_ServiceManager(object):
@@ -25,30 +27,74 @@ class Ui_ServiceManager(object):
         ServiceManager.resize(862, 552)
         self.verticalLayout = QVBoxLayout(ServiceManager)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label = QLabel(ServiceManager)
-        self.label.setObjectName(u"label")
+        self.title = QLabel(ServiceManager)
+        self.title.setObjectName(u"title")
         font = QFont()
         font.setPointSize(20)
         font.setBold(True)
-        self.label.setFont(font)
+        self.title.setFont(font)
 
-        self.verticalLayout.addWidget(self.label)
+        self.verticalLayout.addWidget(self.title)
 
-        self.tabWidget = QTabWidget(ServiceManager)
-        self.tabWidget.setObjectName(u"tabWidget")
+        self.content = QTabWidget(ServiceManager)
+        self.content.setObjectName(u"content")
         self.sys_level = QWidget()
         self.sys_level.setObjectName(u"sys_level")
-        self.tabWidget.addTab(self.sys_level, "")
+        self.verticalLayout_4 = QVBoxLayout(self.sys_level)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.service_search = QLineEdit(self.sys_level)
+        self.service_search.setObjectName(u"service_search")
+
+        self.horizontalLayout.addWidget(self.service_search)
+
+        self.service_search_button = QPushButton(self.sys_level)
+        self.service_search_button.setObjectName(u"service_search_button")
+
+        self.horizontalLayout.addWidget(self.service_search_button)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.service_list_table = QTableView(self.sys_level)
+        self.service_list_table.setObjectName(u"service_list_table")
+        self.service_list_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
+        self.service_list_table.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+
+        self.horizontalLayout_3.addWidget(self.service_list_table)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.pushButton_2 = QPushButton(self.sys_level)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout_2.addWidget(self.pushButton_2)
+
+
+        self.horizontalLayout_3.addLayout(self.verticalLayout_2)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
+
+        self.service_details_table = QTableWidget(self.sys_level)
+        self.service_details_table.setObjectName(u"service_details_table")
+
+        self.verticalLayout_4.addWidget(self.service_details_table)
+
+        self.content.addTab(self.sys_level, "")
         self.user_level = QWidget()
         self.user_level.setObjectName(u"user_level")
-        self.tabWidget.addTab(self.user_level, "")
+        self.content.addTab(self.user_level, "")
 
-        self.verticalLayout.addWidget(self.tabWidget)
+        self.verticalLayout.addWidget(self.content)
 
 
         self.retranslateUi(ServiceManager)
 
-        self.tabWidget.setCurrentIndex(0)
+        self.content.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(ServiceManager)
@@ -56,8 +102,10 @@ class Ui_ServiceManager(object):
 
     def retranslateUi(self, ServiceManager):
         ServiceManager.setWindowTitle(QCoreApplication.translate("ServiceManager", u"Form", None))
-        self.label.setText(QCoreApplication.translate("ServiceManager", u"Systemd Service Management", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.sys_level), QCoreApplication.translate("ServiceManager", u"System level", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.user_level), QCoreApplication.translate("ServiceManager", u"User level", None))
+        self.title.setText(QCoreApplication.translate("ServiceManager", u"Systemd Service Management", None))
+        self.service_search_button.setText(QCoreApplication.translate("ServiceManager", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("ServiceManager", u"PushButton", None))
+        self.content.setTabText(self.content.indexOf(self.sys_level), QCoreApplication.translate("ServiceManager", u"System level", None))
+        self.content.setTabText(self.content.indexOf(self.user_level), QCoreApplication.translate("ServiceManager", u"User level", None))
     # retranslateUi
 
--- archcentral/ui/views/servicemanager.py
+++ archcentral/ui/views/servicemanager.py
@@ -1,10 +1,40 @@
+from PySide6.QtCore import QSortFilterProxyModel, QThread, Signal
 from PySide6.QtWidgets import QWidget
 from archcentral.ui.designer.servicemanager import Ui_ServiceManager
 from archcentral.controllers.servicemanager_controller import ServiceManagerController
+from archcentral.models.systemd_units_list import SystemdServiceListModel
 
 class ServiceManagerModule(QWidget, Ui_ServiceManager):
+    fetch_systemd_units_signal: Signal = Signal()
+
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(ServiceManager=self)
 
+        self.destroyed.connect(self.cleanup_thread)
+
+        self.thread = QThread()
+
         self.smc: ServiceManagerController = ServiceManagerController()
+        self.smc.moveToThread(self.thread)
+
+        self.thread.start()
+
+        self.smc.services_fetched.connect(self.initalize_service_model)
+        self.fetch_systemd_units_signal.connect(self.smc.list_services)
+        self.fetch_systemd_units_signal.emit()
+
+    def initalize_service_model(self, services):
+        """Initializes the systemd service model with services from the systemd dbus API and populates the service table."""
+        self.service_list_model: SystemdServiceListModel = SystemdServiceListModel(services)
+        self.service_list_proxy_model: QSortFilterProxyModel = QSortFilterProxyModel()
+        self.service_list_proxy_model.setDynamicSortFilter(True)
+        self.service_list_proxy_model.setSourceModel(self.service_list_model)
+        self.service_list_proxy_model.setFilterKeyColumn(2)
+        self.service_list_table.setModel(self.service_list_proxy_model)
+
+    def cleanup_thread(self):
+        """Gracefully stops threads."""
+        if self.thread.isRunning():
+            self.thread.quit()
+            self.thread.wait()
