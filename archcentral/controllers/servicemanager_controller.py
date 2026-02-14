d1e60d14d1f00bd2eabbebff068343696d78a63a
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-02-14 15:01:55 +0100

    improved the way service units are fetched, added basic systemctl caller

--- archcentral/controllers/servicemanager_controller.py
+++ archcentral/controllers/servicemanager_controller.py
@@ -1,10 +1,18 @@
 from PySide6.QtCore import QObject, Signal
 from pydbus import SystemBus # using pydbus instead of QtDBus as its more pythonic and far simpler
-from archcentral.helpers.custom_classes import SystemdServiceInfo
+from archcentral.helpers.custom_classes import SystemdUnitInfo
+from threading import Lock
+
+from archcentral.helpers.qprocesshelper import QProcessHandler
+
+SYSTEMD_DBUS_PATH = "org.freedesktop.systemd1"
 
 class ServiceManagerController(QObject):
     # Signals
     services_fetched: Signal = Signal(list)
+    systemctl_lock_activated: Signal = Signal()
+    systemctl_lock_deactivated: Signal = Signal()
+    systemctl_operation_done: Signal = Signal()
 
     def __init__(self) -> None:
         super().__init__()
@@ -13,13 +21,52 @@ class ServiceManagerController(QObject):
         self.system_bus: SystemBus() = SystemBus()
 
         # Connecting to the systemd dbus API
-        self.systemd_system_bus = self.system_bus.get("org.freedesktop.systemd1")
+        self.systemd_system_bus = self.system_bus.get(SYSTEMD_DBUS_PATH)
+
+        # Lock object to prevent native systemctl lock crashing the qprocesses or working with non-up-to-date data
+        self.systemctl_lock: Lock = Lock()
+
+    def _acquire_systemctl(self) -> bool:
+        """Activates the systemctl lock and retuns True, if its already locked it emits a signal and returns False."""
+        if self.systemctl_lock.acquire(blocking=False):
+            self.systemctl_lock_activated.emit()
+            return True
+        return False
+
+    def _release_systemctl(self) -> None:
+        """Releases the systemctl lock and emits a signal."""
+        self.systemctl_lock.release()
+        self.systemctl_lock_deactivated.emit()
 
     def list_services(self):
         """Fetches systemd services off the systemd API."""
-        unitlist: [list[SystemdServiceInfo]] = []
-        for unit in self.systemd_system_bus.ListUnitsByPatterns([],["*.service"]):
-            unitname, desc, loadstate, activestate, substate, unitfollowed, objpath, queued, jobtype, jobpath = unit
-            unitlist.append(SystemdServiceInfo(unitname, desc, activestate, substate))
+        unitfiles = self.systemd_system_bus.ListUnitFilesByPatterns([],["*.service"])
 
-        self.services_fetched.emit(unitlist)
+        processed_unitlist: list[SystemdUnitInfo] = []
+
+        for unitfile in unitfiles:
+            unitname, enabledstate = unitfile
+            if enabledstate in ("disabled", "enabled"):
+                bare_unitname = unitname.split("/")[-1]
+                if "@." not in bare_unitname: # @.service units are template units, and as such can't be loaded
+                    unit_dbus_path = self.systemd_system_bus.LoadUnit(bare_unitname)
+                    unit = self.system_bus.get(SYSTEMD_DBUS_PATH, unit_dbus_path)
+                    processed_unitlist.append(SystemdUnitInfo(unit, unit_dbus_path))
+
+        self.services_fetched.emit(processed_unitlist)
+
+    def call_systemctl(self, unit: str, operation: str) -> None:
+        """
+        Calls systemctl.
+
+        Arguments:
+            - unit: Name of the unit. Eg: "cups.service", ".service" can be omitted.
+            - operation: The type of operation systemctl runs. "enable", "disable", "start", "stop", "restart"
+        """
+        if not self._acquire_systemctl():
+            return None
+
+        self.systemctl_worker: QProcessHandler = QProcessHandler()
+        self.systemctl_worker.finished.connect(self._release_systemctl)
+        self.systemctl_worker.finished.connect(lambda: self.systemctl_operation_done.emit())
+        self.systemctl_worker.start_process("systemctl", [operation, unit])
