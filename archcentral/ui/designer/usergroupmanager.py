ae8b93877e87ece2fdb2040f2decdaf45a8528b9
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-03-15 16:28:58 +0100

    added user adding functionality

--- archcentral/ui/designer/usergroupmanager.py
+++ archcentral/ui/designer/usergroupmanager.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QHeaderView, QLabel,
-    QSizePolicy, QTabWidget, QTableView, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QHBoxLayout, QHeaderView,
+    QLabel, QPushButton, QSizePolicy, QTabWidget,
+    QTableView, QVBoxLayout, QWidget)
 
 class Ui_UserGroupManager(object):
     def setupUi(self, UserGroupManager):
@@ -35,13 +35,13 @@ class Ui_UserGroupManager(object):
 
         self.verticalLayout.addWidget(self.title)
 
-        self.content = QTabWidget(UserGroupManager)
-        self.content.setObjectName(u"content")
-        self.users = QWidget()
-        self.users.setObjectName(u"users")
-        self.verticalLayout_2 = QVBoxLayout(self.users)
+        self.users_groups_tab = QTabWidget(UserGroupManager)
+        self.users_groups_tab.setObjectName(u"users_groups_tab")
+        self.users_tab = QWidget()
+        self.users_tab.setObjectName(u"users_tab")
+        self.verticalLayout_2 = QVBoxLayout(self.users_tab)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.user_list_table = QTableView(self.users)
+        self.user_list_table = QTableView(self.users_tab)
         self.user_list_table.setObjectName(u"user_list_table")
         self.user_list_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
         self.user_list_table.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
@@ -50,12 +50,37 @@ class Ui_UserGroupManager(object):
 
         self.verticalLayout_2.addWidget(self.user_list_table)
 
-        self.content.addTab(self.users, "")
-        self.groups = QWidget()
-        self.groups.setObjectName(u"groups")
-        self.verticalLayout_3 = QVBoxLayout(self.groups)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.add_user_button = QPushButton(self.users_tab)
+        self.add_user_button.setObjectName(u"add_user_button")
+
+        self.horizontalLayout.addWidget(self.add_user_button)
+
+        self.modify_user_button = QPushButton(self.users_tab)
+        self.modify_user_button.setObjectName(u"modify_user_button")
+
+        self.horizontalLayout.addWidget(self.modify_user_button)
+
+        self.delete_user_button = QPushButton(self.users_tab)
+        self.delete_user_button.setObjectName(u"delete_user_button")
+
+        self.horizontalLayout.addWidget(self.delete_user_button)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.users_groups_tab.addTab(self.users_tab, "")
+        self.groups_tab = QWidget()
+        self.groups_tab.setObjectName(u"groups_tab")
+        self.verticalLayout_3 = QVBoxLayout(self.groups_tab)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.group_list_table = QTableView(self.groups)
+        self.label = QLabel(self.groups_tab)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout_3.addWidget(self.label)
+
+        self.group_list_table = QTableView(self.groups_tab)
         self.group_list_table.setObjectName(u"group_list_table")
         self.group_list_table.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
         self.group_list_table.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
@@ -64,14 +89,34 @@ class Ui_UserGroupManager(object):
 
         self.verticalLayout_3.addWidget(self.group_list_table)
 
-        self.content.addTab(self.groups, "")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.manage_users_button = QPushButton(self.groups_tab)
+        self.manage_users_button.setObjectName(u"manage_users_button")
 
-        self.verticalLayout.addWidget(self.content)
+        self.horizontalLayout_2.addWidget(self.manage_users_button)
+
+        self.create_group_button = QPushButton(self.groups_tab)
+        self.create_group_button.setObjectName(u"create_group_button")
+
+        self.horizontalLayout_2.addWidget(self.create_group_button)
+
+        self.delete_group_button = QPushButton(self.groups_tab)
+        self.delete_group_button.setObjectName(u"delete_group_button")
+
+        self.horizontalLayout_2.addWidget(self.delete_group_button)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.users_groups_tab.addTab(self.groups_tab, "")
+
+        self.verticalLayout.addWidget(self.users_groups_tab)
 
 
         self.retranslateUi(UserGroupManager)
 
-        self.content.setCurrentIndex(0)
+        self.users_groups_tab.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(UserGroupManager)
@@ -80,7 +125,14 @@ class Ui_UserGroupManager(object):
     def retranslateUi(self, UserGroupManager):
         UserGroupManager.setWindowTitle(QCoreApplication.translate("UserGroupManager", u"Form", None))
         self.title.setText(QCoreApplication.translate("UserGroupManager", u"User and groups manager", None))
-        self.content.setTabText(self.content.indexOf(self.users), QCoreApplication.translate("UserGroupManager", u"Users", None))
-        self.content.setTabText(self.content.indexOf(self.groups), QCoreApplication.translate("UserGroupManager", u"Groups", None))
+        self.add_user_button.setText(QCoreApplication.translate("UserGroupManager", u"Add user", None))
+        self.modify_user_button.setText(QCoreApplication.translate("UserGroupManager", u"Modify user", None))
+        self.delete_user_button.setText(QCoreApplication.translate("UserGroupManager", u"Delete user", None))
+        self.users_groups_tab.setTabText(self.users_groups_tab.indexOf(self.users_tab), QCoreApplication.translate("UserGroupManager", u"Users", None))
+        self.label.setText(QCoreApplication.translate("UserGroupManager", u"Warning: You shouldn't delete groups you didn't make yourself.", None))
+        self.manage_users_button.setText(QCoreApplication.translate("UserGroupManager", u"Manage users", None))
+        self.create_group_button.setText(QCoreApplication.translate("UserGroupManager", u"Create group", None))
+        self.delete_group_button.setText(QCoreApplication.translate("UserGroupManager", u"Delete group", None))
+        self.users_groups_tab.setTabText(self.users_groups_tab.indexOf(self.groups_tab), QCoreApplication.translate("UserGroupManager", u"Groups", None))
     # retranslateUi
 
--- archcentral/ui/views/usergroupmanager.py
+++ archcentral/ui/views/usergroupmanager.py
@@ -1,13 +1,16 @@
 from typing import Literal
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import QModelIndex, QSortFilterProxyModel, QThread, Signal, Qt
+from archcentral.helpers.custom_classes import GroupInfo, UserInfo
 from archcentral.ui.designer.usergroupmanager import Ui_UserGroupManager
 from archcentral.controllers.usergroupmanager_controller import UserGroupManagerController
 from archcentral.models.user_list import UserListModel
 from archcentral.models.group_list import GroupListModel
+from archcentral.ui.views.dialogs.user_add_dialog import UserAddDialog
 
 class UserManagerModule(QWidget, Ui_UserGroupManager):
-    fetch_data_signal: Signal = Signal()
+    create_user_signal: Signal = Signal(str, str, str, str, str, str)
+    fetch_data_signal: Signal = Signal(bool)
 
     def __init__(self) -> None:
         super().__init__()
@@ -22,10 +25,28 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
 
         self.thread.start()
 
-        self.ugmc.fetched_list.connect(self.initialize_model)
+        self.ugmc.fetched_list_for_init.connect(self.initialize_model)
+        self.ugmc.fetched_list_for_refresh.connect(self.refresh_lists)
 
         self.fetch_data_signal.connect(self.ugmc.fetch_users_and_groups)
-        self.fetch_data_signal.emit()
+        self.fetch_data_signal.emit(False)
+
+        self.ugmc.fetched_available_shells.connect(self.open_user_add_dialog)
+        self.add_user_button.clicked.connect(self.ugmc.get_shell_list)
+
+        self.create_user_signal.connect(self.ugmc.user_add)
+
+    def _get_selected_row(self) -> UserInfo | GroupInfo:
+        """Fetches the object stored in the selected row."""
+        match self.users_groups_tab.currentIndex():
+            case 0:
+                source_index: QModelIndex = self.user_list_proxy_model.mapToSource(self.user_list_table.currentIndex())
+                selected_service = self.user_list_model.get_user_info_by_index(source_index)
+            case 1:
+                source_index: QModelIndex = self.groups_list_proxy_model.mapToSource(self.group_list_table.currentIndex())
+                selected_service = self.groups_list_model.get_group_info_by_index(source_index)
+
+        return selected_service
 
     def initialize_model(self, data, data_type: Literal["users", "groups"]) -> None:
         """Initializes the selected model with relevant data."""
@@ -49,6 +70,34 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
                 self.groups_list_proxy_model.setFilterKeyColumn(0)
 
                 self.group_list_table.setModel(self.groups_list_proxy_model)
+
+    def refresh_lists(self, new_data, data_type: Literal["users", "groups"]) -> None:
+        """Refreshes the user and/or group lists with the given data."""
+        match data_type:
+            case "users":
+                    self.user_list_model.refresh(new_data)
+            case "groups":
+                    self.groups_list_model.refresh(new_data)
+
+    def open_user_add_dialog(self, available_shells) -> None:
+        """Pops up a user creation window."""
+        users: list[str] = self.user_list_model.return_all_users(name_only=True)
+        dialog: UserAddDialog = UserAddDialog(users, available_shells, parent=self)
+        result = dialog.exec()
+        match result:
+            case 0:
+                print("aborted")
+            case 1:
+                self.create_user_signal.emit(
+                    dialog.current_edited_user.username,
+                    dialog.current_edited_user.fullname,
+                    dialog.current_edited_user.homedir,
+                    dialog.current_edited_user.homedirtype,
+                    dialog.current_edited_user.password,
+                    dialog.current_edited_user.shell
+                )
+            case _:
+                print("something went wrong")
 
     def cleanup_thread(self) -> None:
         """Gracefully stops threads."""
