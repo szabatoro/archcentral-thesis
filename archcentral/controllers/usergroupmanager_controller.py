67f17d8ba8f68113b15bad087768660556470aa8
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-03-31 10:53:39 +0200

    Replaced manual db parsing with built-in python libs, added methods for changing user properties

--- archcentral/controllers/usergroupmanager_controller.py
+++ archcentral/controllers/usergroupmanager_controller.py
@@ -2,50 +2,44 @@ from typing import Literal
 from PySide6.QtCore import QObject,  Signal
 from archcentral.helpers.qprocesshelper import QProcessHandler
 from archcentral.helpers.custom_classes import UserInfo, GroupInfo
+from pwd import getpwall
+from grp import getgrall
 
 class UserGroupManagerController(QObject):
-    fetched_list_for_init: Signal = Signal(list, str)
-    fetched_list_for_refresh: Signal = Signal(list, str)
-    fetched_available_shells: Signal = Signal(str)
-    created_user_signal: Signal = Signal()
+    fetched_list_for_init: Signal = Signal(list, list)
+    fetched_list_for_refresh: Signal = Signal(list, list)
+    fetched_available_shells_for_user_creation: Signal = Signal(str)
+    fetched_available_shells_for_shell_change: Signal = Signal(str)
+    created_user_signal: Signal = Signal(str)
+    changed_user_properties_signal: Signal = Signal()
+
+    # These signals carry the user name and the underlying process call's exit code
+    deleted_user_signal: Signal = Signal(str, int)
+    changed_password_signal: Signal = Signal(str, int)
+    changed_shell_signal: Signal = Signal(str, int)
+    changed_full_name_signal: Signal = Signal(str, int)
+    changed_homedir_signal: Signal = Signal(str, int)
 
     def __init__(self) -> None:
         super().__init__()
 
+        # Updates the user and group models after creating a new user or modifying an existing one
         self.created_user_signal.connect(lambda: self.fetch_users_and_groups(for_refresh=True))
+        self.changed_user_properties_signal.connect(lambda: self.fetch_users_and_groups(for_refresh=True))
 
     def fetch_users_and_groups(self, for_refresh: bool) -> None:
-        """Fetches users and groups from /etc/passwd and /etc/group respectively."""
-        self.user_process_handler: QProcessHandler = QProcessHandler()
-        self.user_process_handler.finished.connect(lambda x: self._return_users_or_groups(x, "users", for_refresh))
-        self.user_process_handler.start_process("getent", ["passwd"])
-
-        self.groups_process_handler: QProcessHandler = QProcessHandler()
-        self.groups_process_handler.finished.connect(lambda x: self._return_users_or_groups(x, "groups", for_refresh))
-        self.groups_process_handler.start_process("getent", ["group"])
-
-    def _return_users_or_groups(self, raw_data: str, processtype: Literal["users", "groups"], for_refresh: bool) -> None:
         """Process data provided by fetch_users_or_groups"""
-        # Split the getent output up line by line for further processing
-        sorted_data: list[str] = raw_data.splitlines()
-        # Prepare the list the data will be returned in
-        final_data: list= []
-        match processtype:
-            case "groups":
-                # Process the sorted data line by line for needed information
-                for line in sorted_data:
-                    processed_line: list[str] = line.split(":")
-                    processed_users: list[str] = processed_line[-1].split(",")
-                    final_data.append(GroupInfo(processed_line[0], processed_users))
-                self.fetched_list_for_refresh.emit(final_data, processtype) if for_refresh else self.fetched_list_for_init.emit(final_data, processtype)
-            case "users":
-                # Process the sorted data line by line for needed information
-                for line in sorted_data:
-                    processed_line: list[str] = line.split(":")
-                    # Only list normal users (UID>1000), don't list "nobody" user
-                    if (int)(processed_line[2]) >= 1000 and processed_line[0] != "nobody":
-                        final_data.append(UserInfo(processed_line[0], processed_line[2], processed_line[3], processed_line[4], processed_line[5], processed_line[6]))
-                self.fetched_list_for_refresh.emit(final_data, processtype) if for_refresh else self.fetched_list_for_init.emit(final_data, processtype)
+        users_raw = getpwall()
+        groups_raw = getgrall()
+        processed_groups: list = [
+            GroupInfo(group.gr_name, group.gr_mem) for group in groups_raw
+        ]
+        processed_users = [
+            UserInfo(user.pw_name, user.pw_uid, user.pw_gid, user.pw_gecos, user.pw_dir, user.pw_shell)
+            for user in users_raw
+            if user.pw_uid >= 1000 and user.pw_name != "nobody"
+        ]
+        self.fetched_list_for_refresh.emit(processed_users, processed_groups) if for_refresh else self.fetched_list_for_init.emit(processed_users, processed_groups)
 
     def _get_groups_of_user(self) -> None:
         pass
@@ -53,7 +47,7 @@ class UserGroupManagerController(QObject):
     def user_add(self, username: str, fullname: str,  homedir: str, homedirtype: Literal["none", "auto", "selectexisting"], password: str, shell: str) -> None:
         """Calls the useradd comments with the appropriate parameters, then calls passwd to set the new user's password."""
         self.useradd_worker: QProcessHandler = QProcessHandler()
-        self.useradd_worker.finished.connect(lambda: self.passwd(username, password))
+        self.useradd_worker.finished.connect(lambda: self.passwd(username, password, for_user_creation=True))
         match homedirtype:
             case "selectexisting":
                 arguments = ["useradd", "-d", homedir,"-s", shell, "-c", fullname, username]
@@ -64,15 +58,49 @@ class UserGroupManagerController(QObject):
 
         self.useradd_worker.start_process("pkexec", arguments)
 
-    def passwd(self, username: str, password: str) -> None:
+    def passwd(self, username: str, password: str, for_user_creation: bool = False) -> None:
         """Sets the given user's password."""
         self.passwd_worker: QProcessHandler = QProcessHandler()
-        self.passwd_worker.start_process("pkexec", ["passwd", username, "--stdin"])
         self.passwd_worker.write_to_stdin(f"{password}\n")
-        self.passwd_worker.finished.connect(lambda: self.created_user_signal.emit())
+        if for_user_creation:
+            self.passwd_worker.finished.connect(lambda: self.created_user_signal.emit(username))
+        else:
+            self.passwd_worker.finished_with_exit_code.connect(lambda _, exit_code: self.changed_password_signal.emit(username, exit_code))
+            self.passwd_worker.finished.connect(lambda: self.changed_user_properties_signal.emit())
 
-    def get_shell_list(self) -> None:
+        self.passwd_worker.start_process("pkexec", ["passwd", username, "--stdin"])
+
+    def get_shell_list(self, for_user_creation: bool = False) -> None:
         """Runs chsh -l to get a list of available shells."""
         self.chsh_process_handler: QProcessHandler = QProcessHandler()
-        self.chsh_process_handler.finished.connect(self.fetched_available_shells.emit)
+        self.chsh_process_handler.finished.connect(self.fetched_available_shells_for_user_creation.emit if for_user_creation else self.fetched_available_shells_for_shell_change.emit)
         self.chsh_process_handler.start_process("chsh", ["-l"])
+
+    def change_shell(self, username: str, shell: str) -> None:
+        """Runs chsh -s to set new shell for given user."""
+        self.usermod_shell_worker: QProcessHandler = QProcessHandler()
+        self.usermod_shell_worker.finished_with_exit_code.connect(lambda _, exit_code: self.changed_shell_signal.emit(username, exit_code))
+        self.usermod_shell_worker.finished.connect(lambda: self.changed_user_properties_signal.emit())
+        self.usermod_shell_worker.start_process("pkexec", ["usermod", "-s", shell, username])
+
+    def user_delete(self, username: str, homedir_delete: bool) -> None:
+        """Runs userdel to delete a given user, with the option to also delete their home directory."""
+        self.userdel_worker: QProcessHandler = QProcessHandler()
+        self.userdel_worker.finished_with_exit_code.connect(lambda _, exit_code: self.deleted_user_signal.emit(username, exit_code))
+        self.userdel_worker.finished.connect(lambda: self.changed_user_properties_signal.emit())
+        userdel_args = ["userdel", "-r", username] if homedir_delete else ["userdel", username]
+        self.userdel_worker.start_process("pkexec", userdel_args)
+
+    def change_full_name(self, username: str, full_name: str) -> None:
+        """Runs usermod -c to change user's full name (GECOS comment)."""
+        self.usermod_gecos_worker: QProcessHandler = QProcessHandler()
+        self.usermod_gecos_worker.finished_with_exit_code.connect(lambda _, exit_code: self.changed_full_name_signal.emit(username, exit_code))
+        #self.usermod_gecos_worker.finished.connect(lambda: self.changed_user_properties_signal.emit())
+        self.usermod_gecos_worker.start_process("pkexec", ["usermod", "-c", full_name, username])
+
+    def change_homedir(self, username: str, homedir: str) -> None:
+        """Runs usermod -d to change the user's home directory."""
+        self.usermod_homedir_worker: QProcessHandler = QProcessHandler()
+        self.usermod_homedir_worker.finished_with_exit_code.connect(lambda _, exit_code: self.changed_homedir_signal.emit(username, exit_code))
+        self.usermod_homedir_worker.finished.connect(lambda: self.changed_user_properties_signal.emit())
+        self.usermod_homedir_worker.start_process("pkexec", ["usermod", "-d", homedir, username])
