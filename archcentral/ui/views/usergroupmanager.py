d64205089108a62feaeb20ce5dbd1b9f885f2268
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-04-01 18:30:16 +0200

    Integrated the group management dialogs to the usergroupmanager module

--- archcentral/ui/views/usergroupmanager.py
+++ archcentral/ui/views/usergroupmanager.py
@@ -5,6 +5,8 @@ from archcentral.ui.designer.usergroupmanager import Ui_UserGroupManager
 from archcentral.controllers.usergroupmanager_controller import UserGroupManagerController
 from archcentral.models.user_list import UserListModel
 from archcentral.models.group_list import GroupListModel
+from archcentral.ui.views.dialogs.group_add_dialog import GroupAddDialog
+from archcentral.ui.views.dialogs.group_delete_dialog import GroupDeleteDialog
 from archcentral.ui.views.dialogs.group_manage_users_dialog import GroupManageUsersDialog
 from archcentral.ui.views.dialogs.user_add_dialog import UserAddDialog
 from archcentral.ui.views.dialogs.user_delete_dialog import UserDeleteDialog
@@ -14,14 +16,17 @@ from archcentral.ui.views.dialogs.user_password_change_dialog import UserPasswor
 from archcentral.ui.views.dialogs.user_shell_change_dialog import UserChangeShellDialog
 
 class UserManagerModule(QWidget, Ui_UserGroupManager):
-    create_user_signal: Signal = Signal(str, str, str, str, str, str)
+    create_user_signal: Signal = Signal(str, str, str, str, str, str, list)
     delete_user_signal: Signal = Signal(str, bool)
+    create_group_signal: Signal = Signal(str, bool, list)
+    delete_group_signal: Signal = Signal(str)
     fetch_data_signal: Signal = Signal(bool) # True for refreshing, False for initalizing
     fetch_shells_signal: Signal = Signal(bool)
     change_user_password_signal: Signal = Signal(str, str)
     change_user_shell_signal: Signal = Signal(str, str)
     change_user_full_name_signal: Signal = Signal(str, str)
     change_user_homedir_signal: Signal = Signal(str, str)
+    modify_group_users_signal: Signal = Signal(str, list)
 
     def __init__(self) -> None:
         super().__init__()
@@ -41,6 +46,9 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
         self.ugmc.fetched_list_for_refresh.connect(self.refresh_lists)
 
         # User modification status messages
+        self.ugmc.created_user_signal.connect(
+            lambda user, exit_code: self.status_label.setText(f"{user} created." if exit_code == 0 else "User creation aborted.")
+        )
         self.ugmc.changed_password_signal.connect(
             lambda user, exit_code: self.status_label.setText(f"Password changed for user {user}." if exit_code == 0 else "Password change aborted.")
         )
@@ -55,6 +63,15 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
         )
         self.ugmc.deleted_user_signal.connect(
             lambda user, exit_code: self.status_label.setText(f"{user} successfully deleted." if exit_code == 0 else "User deletion aborted.")
+        )
+        self.ugmc.changed_group_members_signal.connect(
+            lambda group, exit_code: self.status_label.setText(f"Group member changes in {group} group performed successfully." if exit_code == 0 else "Group member changes aborted.")
+        )
+        self.ugmc.created_group_signal.connect(
+            lambda group, exit_code: self.status_label.setText(f"{group} group created successfully." if exit_code == 0 else "Group creation aborted.")
+        )
+        self.ugmc.deleted_group_signal.connect(
+            lambda group, exit_code: self.status_label.setText(f"{group} group deleted successfully." if exit_code == 0 else "Group deletion aborted.")
         )
 
         # Calls data fetcher controller function to initialize the models
@@ -80,12 +97,18 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
 
         self.manage_users_button.clicked.connect(self.open_manage_group_members_dialog)
 
+        self.create_group_button.clicked.connect(self.open_create_group_dialog)
+
+        self.delete_group_button.clicked.connect(self.open_group_deletion_dialog)
+
         # Connect signals emitted after finishing the dialogs to their respective controller functions
         self.create_user_signal.connect(self.ugmc.user_add)
         self.change_user_password_signal.connect(self.ugmc.passwd)
         self.change_user_shell_signal.connect(self.ugmc.change_shell)
         self.change_user_full_name_signal.connect(self.ugmc.change_full_name)
         self.change_user_homedir_signal.connect(self.ugmc.change_homedir)
+        self.modify_group_users_signal.connect(self.ugmc.modify_group_users)
+        self.create_group_signal.connect(self.ugmc.group_add)
 
     def _get_selected_row(self) -> UserInfo | GroupInfo:
         """Returns the object connected to the selected row from the model."""
@@ -126,7 +149,7 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
 
     def open_user_add_dialog(self, available_shells) -> None:
         """Pops up a user creation window."""
-        users: list[str] = self.user_list_model.return_all_users(name_only=True)
+        users: list[str] = self.user_list_model.return_all_users()
         groups: list[GroupInfo] = self.group_list_model.return_all_groups(name_only=True)
         dialog: UserAddDialog = UserAddDialog(users, available_shells, groups, parent=self)
         result = dialog.exec()
@@ -141,7 +164,8 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
                     dialog.current_edited_user.homedir,
                     dialog.current_edited_user.homedirtype,
                     dialog.current_edited_user.password,
-                    dialog.current_edited_user.shell
+                    dialog.current_edited_user.shell,
+                    dialog.current_edited_user.groups
                 )
             case _:
                 self.status_label.setText("Adding user aborted.")
@@ -196,7 +220,11 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
 
     def open_user_change_homedir_dialog(self) -> None:
         """Pops up home directory change dialog."""
-        dialog: UserChangeHomeDirDialog = UserChangeHomeDirDialog(selected_user=self._get_selected_row(), parent=self)
+        dialog: UserChangeHomeDirDialog = UserChangeHomeDirDialog(
+            selected_user=self._get_selected_row(),
+            userlist=self.user_list_model.return_all_users(),
+            parent=self
+        )
         result = dialog.exec()
         match result:
             case 0:
@@ -236,13 +264,50 @@ class UserManagerModule(QWidget, Ui_UserGroupManager):
             case 0:
                 self.status_label.setText("Group member changes aborted.")
             case 1:
-                self.status_label.setText("Group member changes in process...")
-                self.change_user_password_signal.emit(
-                    dialog.user,
-                    dialog.password
-                )
+                if dialog.marked_users:
+                    self.status_label.setText("Group member changes in process...")
+                    self.modify_group_users_signal.emit(
+                        dialog.group.name,
+                        dialog.marked_users
+                    )
             case _:
                 self.status_label.setText("Group member changes aborted.")
+
+    def open_create_group_dialog(self) -> None:
+        """Pops up group creation dialog."""
+        dialog: GroupAddDialog = GroupAddDialog(
+            groups=self.group_list_model.return_all_groups(name_only=True),
+            userlist=self.user_list_model.return_all_users(name_only=True),
+            parent=self
+        )
+        result = dialog.exec()
+        match result:
+            case 0:
+                self.status_label.setText("Group creation aborted.")
+            case 1:
+                self.status_label.setText("Group creation in process...")
+                self.create_group_signal.emit(
+                     dialog.current_edited_group.group_name,
+                     dialog.current_edited_group.is_system_group,
+                     dialog.current_edited_group.users
+                )
+            case _:
+                self.status_label.setText("Group creation aborted.")
+
+    def open_group_deletion_dialog(self) -> None:
+        """Pops up user deletion dialog."""
+        dialog: GroupDeleteDialog = GroupDeleteDialog(selected_group=self._get_selected_row(), parent=self)
+        result = dialog.exec()
+        match result:
+            case 0:
+                self.status_label.setText("Group deletion aborted.")
+            case 1:
+                self.status_label.setText("Group deletion in progress...")
+                self.delete_group_signal.emit(
+                    dialog.group
+                )
+            case _:
+                self.status_label.setText("Group deletion aborted.")
 
     def cleanup_thread(self) -> None:
         """Gracefully stops threads."""
