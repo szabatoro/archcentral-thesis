872213fe83548cdb66b3cc6e9f972cd88199818c
szabatoro <99820354+szabatoro@users.noreply.github.com>
2026-04-22 10:46:09 +0200

    fixed inconsistencies and the resulting errors in servicemanager

--- archcentral/controllers/servicemanager_controller.py
+++ archcentral/controllers/servicemanager_controller.py
@@ -17,7 +17,7 @@ class ServiceManagerController(QObject):
     sockets_fetched_for_refresh: Signal = Signal(bool, list)
     systemctl_lock_activated: Signal = Signal()
     systemctl_lock_deactivated: Signal = Signal()
-    systemctl_operation_done: Signal = Signal(str)
+    systemctl_operation_done: Signal = Signal(bool, str)
 
     def __init__(self) -> None:
         super().__init__()
@@ -33,7 +33,7 @@ class ServiceManagerController(QObject):
         # Lock object to prevent native systemctl lock crashing the qprocesses or working with non-up-to-date data
         self.systemctl_lock: Lock = Lock()
 
-        self.systemctl_operation_done.connect(lambda type: self.list_units(True, type))
+        self.systemctl_operation_done.connect(lambda is_user_unit, type: self.list_units(True, is_user_unit, type))
 
     def _acquire_systemctl(self) -> bool:
         """Activates the systemctl lock and retuns True, if its already locked it emits a signal and returns False."""
@@ -47,10 +47,10 @@ class ServiceManagerController(QObject):
         self.systemctl_lock.release()
         self.systemctl_lock_deactivated.emit()
 
-    def _list_units_internal(self, unit_type: Literal["service", "timer", "socket"], is_system_level: bool, ) -> list:
+    def _list_units_internal(self, unit_type: Literal["service", "timer", "socket"], is_user_level: bool, ) -> list:
         """Fetches systemd services off the systemd API."""
-        bus = self.system_bus if is_system_level else self.user_bus
-        systemd_bus = self.systemd_system_bus if is_system_level else self.systemd_user_bus
+        bus = self.user_bus if is_user_level else self.system_bus
+        systemd_bus = self.systemd_user_bus if is_user_level else self.systemd_system_bus
         unitfiles = systemd_bus.ListUnitFilesByPatterns([],[f"*.{unit_type}"])
 
         processed_unitlist: list = []
@@ -72,26 +72,26 @@ class ServiceManagerController(QObject):
 
         return processed_unitlist
 
-    def list_units(self, is_for_refresh: bool, is_system_level: bool, unit_type: Literal["service", "timer", "socket"]) -> None:
+    def list_units(self, is_for_refresh: bool, is_user_level: bool, unit_type: Literal["service", "timer", "socket"]) -> None:
         """Returns unit lists for their respective categories."""
         self._acquire_systemctl()
-        units: list[SystemdServiceInfo] = self._list_units_internal(unit_type, is_system_level)
+        units: list[SystemdServiceInfo] = self._list_units_internal(unit_type, is_user_level)
         if is_for_refresh:
             match unit_type:
                 case "service":
-                        self.services_fetched_for_refresh.emit(is_system_level, units)
+                        self.services_fetched_for_refresh.emit(is_user_level, units)
                 case "timer":
-                        self.timers_fetched_for_refresh.emit(is_system_level, units)
+                        self.timers_fetched_for_refresh.emit(is_user_level, units)
                 case "socket":
-                        self.sockets_fetched_for_refresh.emit(is_system_level, units)
+                        self.sockets_fetched_for_refresh.emit(is_user_level, units)
         else:
             match unit_type:
                 case "service":
-                        self.services_fetched_for_init.emit(is_system_level, units)
+                        self.services_fetched_for_init.emit(is_user_level, units)
                 case "timer":
-                        self.timers_fetched_for_init.emit(is_system_level, units)
+                        self.timers_fetched_for_init.emit(is_user_level, units)
                 case "socket":
-                        self.sockets_fetched_for_init.emit(is_system_level, units)
+                        self.sockets_fetched_for_init.emit(is_user_level, units)
 
         self._release_systemctl()
 
@@ -109,6 +109,6 @@ class ServiceManagerController(QObject):
             return None
 
         self.systemctl_worker: QProcessHandler = QProcessHandler()
-        self.systemctl_worker.finished.connect(lambda: self.systemctl_operation_done.emit(unit_type))
+        self.systemctl_worker.finished.connect(lambda: self.systemctl_operation_done.emit(is_user_unit, unit_type))
         systemctl_args = ["--user", operation, unit] if is_user_unit else [operation, unit]
         self.systemctl_worker.start_process("systemctl", systemctl_args)
